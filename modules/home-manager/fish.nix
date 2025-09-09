89db08adbc77a223eacd7f6d0ea9c6aff5a8df69
Joona Kärkkäinen <joona.karkkainen@gmail.com>
2025-09-09 17:45:39 +0300

    feat: fish vi mode

--- modules/home-manager/fish.nix
+++ modules/home-manager/fish.nix
@@ -16,7 +16,15 @@
     ];
 
     shellInitLast = ''
+      # Disable shell greeting
+      set -U fish_greeting
+
+      # SSH authentication via Bitwarden
       set -gx SSH_AUTH_SOCK "/Users/joona/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock";
+
+      # Vi mode
+      set -g fish_key_bindings fish_vi_key_bindings
+      bind -M command ctrl-e edit_command_buffer
     '';
   };
 }
