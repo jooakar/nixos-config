7dc0cf88c3428d724362c3685e05c874767d2530
Joona Kärkkäinen <jook@netlight.com>
2026-07-07 10:53:53 +0300

    feat(fish): add script for sourcing .env files

--- modules/home-manager/fish.nix
+++ modules/home-manager/fish.nix
@@ -20,6 +20,26 @@
       }
     ];
 
+    functions = {
+      envsource = {
+        description = "Source a .env file into the current shell";
+        body = ''
+          set -f envfile "$argv"
+          if not test -f "$envfile"
+            echo "Unable to load $envfile"
+            return 1
+          end
+          while read line
+            if not string match -qr '^#|^$' "$line"
+              set item (string split -m 1 '=' $line)
+              set -gx $item[1] $item[2]
+              echo "Exported key $item[1]"
+            end
+          end < "$envfile"
+        '';
+      };
+    };
+
     shellInitLast = ''
       # Disable shell greeting
       set -U fish_greeting
