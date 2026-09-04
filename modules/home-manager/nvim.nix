19aa9bdc671d783b1978ec71abd87491fcaa57bd
Joona Kärkkäinen <joona.karkkainen@gmail.com>
2026-09-04 23:21:51 +0300

    feat(nix): add more sensible defaults

--- modules/home-manager/nvim.nix
+++ modules/home-manager/nvim.nix
@@ -6,10 +6,20 @@
     vimAlias = true;
     vimdiffAlias = true;
 
+    withPython3 = false;
+    withRuby = false;
+
     extraConfig = ''
       set clipboard=unnamedplus
       set relativenumber
       set termguicolors
+
+      set expandtab
+      set shiftwidth=2
+      set tabstop=2
+      set softtabstop=2
+      set shiftround
+      set smartindent
     '';
 
     plugins = [
@@ -19,6 +29,14 @@
         config = ''
           require("ayu").setup({ mirage = false })
           vim.cmd.colorscheme("ayu-dark")
+        '';
+      }
+      {
+        plugin = pkgs.vimPlugins.oil-nvim;
+        type = "lua";
+        config = ''
+          require("oil").setup()
+          vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
         '';
       }
     ];
